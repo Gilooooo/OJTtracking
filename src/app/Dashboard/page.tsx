4f8691c0eaf0a4e1473ca4d2ce24a40c05c7929f
Angelo Sabornido <angelosabornido992@gmail.com>
2025-08-24 14:25:34 +0800

    Adding hamburger for nav

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -3,7 +3,7 @@ import Dashboard from "@/_Component/Dashboard/dashboard";
 export default function DashboardPage(){
     return(
         <>
-        <Dashboard/>
+            <Dashboard/>
         </>
     )
 }
